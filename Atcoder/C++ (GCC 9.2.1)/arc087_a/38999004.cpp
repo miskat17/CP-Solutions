a39d426e3ab0ba85d6ab7aeac8b01fdbf31e4986
Miskat Hossain Roney <82561950+miskat17@users.noreply.github.com>
2024-01-01 18:23:58 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/arc087_a/38999004.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define pb push_back
+#define pf push_front
+#define pi 2 * acos(0.0)
+#define endl '\n'
+#define pi 2 * acos(0.0)
+#define mod 1000000007
+#define optimizer()                   \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+int main()
+{
+    optimizer();
+    int n;
+    cin >> n;
+    map<int, int> cnt;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        cnt[a]++;
+    }
+    int ans = 0;
+    for (auto u : cnt)
+    {
+        if (u.second >= u.first)
+            ans += (u.second - u.first);
+        else
+            ans += u.second;
+    }
+    cout << ans << endl;
+
+    return 0;
+}
